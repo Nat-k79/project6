e3df9ef08b62f6719ea2e7977aed369ae2066822
Admin <natalia.zorina.k@gmail.com>
2025-04-24 21:45:39 +0300

    fixes for dev

--- src/processing.py
+++ src/processing.py
@@ -5,9 +5,6 @@ def filter_by_state(transactions: List[Dict], state: str = 'EXECUTED') -> List[D
     """
     Функция фильтрует список словарей по значению ключа 'state'.
 
-    :param transactions: Список словарей с данными о банковских операциях.
-    :param state: Значение ключа 'state', по которому будет производиться фильтрация. По умолчанию 'EXECUTED'.
-    :return: Новый список словарей, содержащий только те, у которых ключ 'state' соответствует указанному значению.
     """
     return [transaction for transaction in transactions if transaction.get('state') == state]
 
@@ -16,8 +13,5 @@ def sort_by_date(transactions: List[Dict], descending: bool = True) -> List[Dict
     """
     Функция сортирует список словарей по дате.
 
-    :param transactions: Список словарей с данными о банковских операциях.
-    :param descending: Параметр, задающий порядок сортировки. По умолчанию True (по убыванию).
-    :return: Новый список словарей, отсортированный по дате.
     """
     return sorted(transactions, key=lambda x: x['date'], reverse=descending)
