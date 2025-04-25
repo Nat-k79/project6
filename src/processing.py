1fc71b4b2fd6171db4f81dd019d4cf2d07b984af
Admin <natalia.zorina.k@gmail.com>
2025-04-25 07:58:28 +0300

    Добавлено исправленное замечание по домашке

--- src/processing.py
+++ src/processing.py
@@ -19,3 +19,24 @@ def sort_by_date(transactions: List[Dict], descending: bool = True) -> List[Dict
     :return: Новый список словарей, отсортированный по дате.
     """
     return sorted(transactions, key=lambda x: x['date'], reverse=descending)
+
+
+if __name__ == "__main__":
+    transactions = [
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    ]
+
+    executed_transactions = filter_by_state(transactions)
+    print("Executed Transactions:", executed_transactions)
+
+    canceled_transactions = filter_by_state(transactions, state='CANCELED')
+    print("Canceled Transactions:", canceled_transactions)
+
+    sorted_transactions = sort_by_date(transactions)
+    print("Sorted Transactions (Descending):", sorted_transactions)
+
+    sorted_transactions_asc = sort_by_date(transactions, descending=False)
+    print("Sorted Transactions (Ascending):", sorted_transactions_asc)
