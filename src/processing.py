8ae1f86ba0291d8d31f17897d1cb84e633ce644b
Admin <natalia.zorina.k@gmail.com>
2025-04-25 06:56:08 +0300

    добвалены Docstring и аннотации типов

--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,12 @@
-from typing import List, Dict, Optional
+from typing import List, Dict
 
 
 def filter_by_state(transactions: List[Dict], state: str = 'EXECUTED') -> List[Dict]:
     """
     Функция фильтрует список словарей по значению ключа 'state'.
-
+    :transactions: Список словарей с данными о банковских операциях.
+    :state: Значение ключа для фильтрации. По умолчанию 'EXECUTED'.
+    :return: Новый список словарей содержит те, у которых ключ 'state' соответствует указанному значению.
     """
     return [transaction for transaction in transactions if transaction.get('state') == state]
 
@@ -12,6 +14,8 @@ def filter_by_state(transactions: List[Dict], state: str = 'EXECUTED') -> List[D
 def sort_by_date(transactions: List[Dict], descending: bool = True) -> List[Dict]:
     """
     Функция сортирует список словарей по дате.
-
+    :transactions: Список словарей с данными о банковских операциях.
+    :descending: Параметр задает порядок сортировки. По умолчанию True (по убыванию).
+    :return: Новый список словарей, отсортированный по дате.
     """
     return sorted(transactions, key=lambda x: x['date'], reverse=descending)
